06af7cebd0f99eb52b1922e990b5daddcadc1a22
0sin0926 <104380887+0sin0926@users.noreply.github.com>
2023-05-25 16:30:46 +0900

    Add files via upload

---
+++ week13/21_p3.cpp
@@ -0,0 +1,37 @@
+// 인접행렬 기반의 무향그래프
+#include <iostream>
+using namespace std;
+
+bool graph[2000][2000];
+int main() {
+	int t, n;
+	int id;
+	cin >> t >> n;
+	for (int i = 0; i < n; i++) { // 결과에 영향 주지 않는 의미 없는 코드
+		cin >> id;
+	}
+
+	while (t--) {
+		int s, d;
+		string cmd;
+		cin >> cmd;
+		if (cmd == "insertEdge") {
+			cin >> s >> d;
+			if (graph[s][d]) cout << "Exist\n";
+			else graph[s][d] = graph[d][s] = true;
+		}
+		else if (cmd == "eraseEdge") {
+			cin >> s >> d;
+			if (!graph[s][d]) cout << "None\n";
+			else graph[s][d] = graph[d][s] = false;
+		}
+		else if (cmd == "incidentEdges") {
+			int cnt = 0;
+			cin >> s;
+			for (int i = 0; i < 2000; i++) {
+				if (graph[s][i]) cnt++;
+			}
+			cout << cnt << "\n";
+		}
+	}
+}
